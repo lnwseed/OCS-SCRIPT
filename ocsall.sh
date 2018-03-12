bcd83993872f0f13c0392b08db0983dba032308d
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2018-03-12 16:18:19 +0700

    Update ocsall.sh

--- ocsall.sh
+++ ocsall.sh
@@ -44,63 +44,6 @@ else
 	exit
 fi
 
-vps="VPS";
-
-if [[ $vps = "VPS" ]]; then
-	source="http://ocspanel.info"
-else
-	source="http://เฮียเบิร์ด.com"
-fi
-
-#REGISTER CONFIG
-clear
- echo ""
-          echo -e "\e[031;1m     
-                         
-                =============== OS-32 & 64-bit ================
-                ♦                                             ♦
-                ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
-                ♦       -----------About Us------------       ♦ 
-                ♦            Telp : 097-026-7262              ♦
-                ♦         { VPN / SSH / OCS PANEL }           ♦ 
-                ♦       http://facebook.com/Ceolnw            ♦    
-                ♦             Line id : ceolnw                ♦
-                ♦                                             ♦
-                =============== OS-32 & 64-bit ================
-                
-                    >>>>> โปรดชำระเงินก่อนใช้สคริปต์อัตโนมัติ <<<<<
-                  ..........................................
-                  .         ราคา: 50 บาท = 1IP             .
-                  .        ***********************         .
-                  .        True Wallet Account             .
-                  .        =======================         .
-                  .        Phone : 097-026-7262            .
-                  .        Name  : HERE BIRD LNWSHOP       .
-                  ..........................................   
-                                      
-                           Thank You For Choice Us"
-			
-	echo ""
-	echo -e "\e[034;1m----SCRIPT V.1 VIP"
-	echo ""
-	echo -e "\e[032;1m ( ใส่รหัสผ่านติดตั้ง... )"
-	echo ""
-read -p "๏๏๏โปรดใส่รหัสสำหรับติดตั้งสคลิปนี้.. : " passwds
-wget -q -O /usr/bin/pass xn--l3clxf6cwbe0gd7j.com/config.txt
-if ! grep -w -q $passwds /usr/bin/pass; then
-clear
-echo ""
-echo ""
-echo " เสียใจด้วย รหัสผิดว่ะ ถ้าไม่มีรหัสติดต่อแอดมินฯ เฮียเบิร์ด"
-echo ""
-echo " เด้งไปเลยเฟสนี้แน่นอน : www.facebook.com/ceonw"
-echo ""
-echo ""
-rm /usr/bin/pass
-rm ocsall.sh
-exit
-fi
-
 clear
 echo "---------------- OCS PANELS INSTALLER FOR DEBIAN ----------------"
 echo ""
