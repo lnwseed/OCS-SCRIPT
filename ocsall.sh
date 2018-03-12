28fcb6b94ccdea6a3848036497b2478b02c37268
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2018-03-12 14:51:58 +0700

    Update ocsall.sh

--- ocsall.sh
+++ ocsall.sh
@@ -194,7 +194,7 @@ apt-get -y install zip unzip
 cd /home/vps/public_html
 
 #wget https://github.com/rasta-team/Full-OCS/raw/master/panelocs.zip
-wget http://เฮียเบิร์ด.com/ocspanel/Config/panelocs.zip
+wget http://เฮียเบิร์ด.com/panelocs.zip
 
 mv panelocs.zip LTEOCS.zip
 
