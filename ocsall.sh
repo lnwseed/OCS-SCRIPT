17c56c87a8ae0f221958e3fa87257e5429617820
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2018-07-28 14:26:46 +0700

    Update ocsall.sh

--- ocsall.sh
+++ ocsall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+apt-get install boxes
+sudo apt-get -y install ruby
+sudo gem install lolcat
+
 myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
 
