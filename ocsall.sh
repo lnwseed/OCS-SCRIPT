3f8733eb91ca6402680449bf46369f2b3dd1c5ef
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2018-08-07 19:25:10 +0700

    Update ocsall.sh

--- ocsall.sh
+++ ocsall.sh
@@ -184,7 +184,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/rasta-team/MyVPS/master/nginx.conf"
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/rasta-team/MyVPS/master/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/lnwseed/ocs-topup/master/vps.conf"
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 
